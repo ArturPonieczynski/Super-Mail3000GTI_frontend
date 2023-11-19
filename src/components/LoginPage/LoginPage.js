296a363ecd5df88537c2eaf037276c755d847891
Artur Ponieczynski <ponieczynski@gmail.com>
2023-11-19 21:14:25 +0100

    Feature: Renamed and moved LoginPage.js to component directory. Added main logo picture. Added toast notification. Handle response and errors.

---
+++ src/components/LoginPage/LoginPage.js
@@ -0,0 +1,76 @@
+import React, {useState} from "react";
+import {apiUrl} from "../../api";
+import {toast} from "react-toastify";
+import {useNavigate} from "react-router-dom";
+import style from "./LoginPage.module.css";
+import logo from "../../img/main512.png";
+
+export function LoginPage() {
+
+    const [form, setForm] = useState({
+        name: '',
+        password: '',
+    });
+
+    const navigate = useNavigate();
+
+    const updateForm = (key, value) => {
+        setForm(form => ({
+            ...form,
+            [key]: value.toLowerCase(),
+        }));
+    };
+
+    const sendForm = async (event) => {
+        event.preventDefault();
+
+        try {
+            const res = await fetch(`${apiUrl}/api/login`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({...form}),
+            });
+
+            const result = await res.json();
+
+            if (result.error) {
+                toast.error(`${result.error}`);
+                return;
+            } if (result.login) {
+                navigate('/mail');
+            }
+
+        } catch (error) {
+            toast.error('Nieprawidłowe dane.');
+            navigate('/login');
+        }
+    };
+
+    return (<div className={style.divContainer}>
+        <img className={style.logo} src={logo} alt="Main logo"/>
+        <form className={style.loginForm} action="" onSubmit={sendForm}>
+            <input
+                className={style.input}
+                type="text"
+                name="name"
+                placeholder="Name"
+                maxLength={50}
+                value={form.name}
+                onChange={e => updateForm('name', e.target.value)}
+                required/>
+            <input
+                className={style.input}
+                type="password"
+                name="password"
+                placeholder="Password"
+                maxLength={45}
+                minLength={4}
+                value={form.password}
+                onChange={e => updateForm('password', e.target.value)}
+                required/>
+            <button className={style.formButton} type="submit">Login</button>
+        </form>
+    </div>)
+}
