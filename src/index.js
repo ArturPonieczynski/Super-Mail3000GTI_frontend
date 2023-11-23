da71aefc298f1e906d3eb21998b7d416ab7a3ff3
Artur Ponieczynski <ponieczynski@gmail.com>
2023-11-23 03:20:22 +0100

    Release: Added Google Analytics and Google tag. Updated app version.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,17 @@ import {App} from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 
+function sendToGoogleAnalytics({ id, name, value }) {
+    window.gtag('event', name, {
+        event_category: 'Web Vitals',
+        value: Math.round(name === 'CLS' ? value * 1000 : value),
+        event_label: id,
+        non_interaction: true,
+    });
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
     <React.StrictMode>
         <BrowserRouter>
@@ -17,4 +27,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(sendToGoogleAnalytics);
