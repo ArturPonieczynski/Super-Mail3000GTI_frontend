da098699b3c73fc23a0e68eec6004ed2304f9c72
Artur Ponieczynski <ponieczynski@gmail.com>
2023-11-27 19:50:50 +0100

    Feature/email-addresses-list: Pre-styled component for multiple elements.

--- src/components/EmailsList/EmailsList.js
+++ src/components/EmailsList/EmailsList.js
@@ -2,11 +2,16 @@ import React from "react";
 import styles from "./EmailsList.module.css";
 
 export const EmailsList = () => {
-    return (
+    return <>
         <div className={styles.checkboxListRecord}>
             <input id="first" type="checkbox" value="mail@test.com" name="test"/>
             <label htmlFor="first">example@mail.com</label>
             <p>mail.com</p>
         </div>
-    )
+        <div className={styles.checkboxListRecord}>
+            <input id="second" type="checkbox" value="mail@test.com" name="test"/>
+            <label htmlFor="second">example@mail.com</label>
+            <p>mailfdgs  sdgdsfg.com</p>
+        </div>
+    </>
 }
