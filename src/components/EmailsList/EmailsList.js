06b548e2bea13f6d7de9e453f2c3453f34634152
Artur Ponieczynski <ponieczynski@gmail.com>
2023-11-28 06:11:20 +0100

    Feature/email-addresses-list: Lifting state up with selected data.

--- src/components/EmailsList/EmailsList.js
+++ src/components/EmailsList/EmailsList.js
@@ -1,9 +1,9 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, {useEffect, useState} from "react";
 import styles from "./EmailsList.module.css";
 import {apiUrl} from "../../config/api";
 import {toast} from "react-toastify";
 
-export const EmailsList = () => {
+export const EmailsList = ({onEmailSelect}) => {
 
     const [members, setMembers] = useState([]);
 
@@ -17,22 +17,26 @@ export const EmailsList = () => {
                 setMembers(result);
             } catch (error) {
                 toast.error('Błąd ładowania książki adresów', {theme: 'colored'});
-                console.log(error);
+                console.error(error);
             }
         };
 
         fetchMembers();
     }, []);
 
-    const emailListContainerRef = useRef(null);
-
-    return <div ref={emailListContainerRef}>
+    return <>
         {members.map((member) => (
             <div key={member.id} className={styles.checkboxListRecord}>
-                <input id={member.id} type="checkbox" value={member.email} name={member.email}/>
+                <input
+                    id={member.id}
+                    type="checkbox"
+                    value={member.email}
+                    name={member.email}
+                    onChange={(event) => onEmailSelect(member.email, event.target.checked)}
+                />
                 <label htmlFor={member.id}>{member.email}</label>
                 <label htmlFor={member.id}>{member.description}</label>
             </div>
         ))}
-    </div>
+    </>
 }
