a1b4a6b2feee677c2f4d435106e7439cf18c17c4
Artur Ponieczynski <ponieczynski@gmail.com>
2023-12-01 23:25:20 +0100

    Feature/selector-for-message-type: Working prototype of EmailsList.js.

--- src/components/EmailsList/EmailsList.js
+++ src/components/EmailsList/EmailsList.js
@@ -6,6 +6,61 @@ import {toast} from "react-toastify";
 export const EmailsList = ({onEmailSelect}) => {
 
     const [members, setMembers] = useState([]);
+    // const [checkedValue, setCheckedValue] = useState(new Map());
+    const [selectedValue, setSelectedValue] = useState({});
+
+
+    // const handleSelectChange = (memberId, value) => {
+    //
+    //     if (typeof value === 'boolean') {
+    //         setSelectedValue(selectedValue => {
+    //             return {...selectedValue, [memberId]:{...selectedValue[memberId],isChecked: value}};
+    //         })
+    //     } else {
+    //         setSelectedValue(selectedValue => {
+    //             return {...selectedValue, [memberId]:{...selectedValue[memberId],method: value}}
+    //         });
+    //     }
+    //     console.log(selectedValue);
+    //     const [email, method, isChecked] = Object.values(selectedValue[memberId]);
+    //     console.log(email, method, !isChecked);
+    //     onEmailSelect(
+    //         email,
+    //         method,
+    //         !isChecked
+    //     )
+    // };
+
+    const handleSelectChange = (memberId, value) => {
+        let email = members.find(member => member.id === memberId).email;
+        let newSelectedValue;
+
+        if (typeof value === 'boolean') {
+            newSelectedValue = {
+                ...selectedValue,
+                [memberId]: {
+                    ...selectedValue[memberId],
+                    isChecked: value
+                }
+            };
+        } else {
+            newSelectedValue = {
+                ...selectedValue,
+                [memberId]: {
+                    ...selectedValue[memberId],
+                    method: value
+                }
+            };
+        }
+
+        setSelectedValue(newSelectedValue);
+        const { method, isChecked } = newSelectedValue[memberId];
+        onEmailSelect(email, method, isChecked);
+    };
+
+    // const handleCheckedChange = (memberId, value) => {
+    //     setCheckedValue(prev => prev.set(memberId, value));
+    // };
 
     useEffect(() => {
         const fetchMembers = async () => {
@@ -15,6 +70,16 @@ export const EmailsList = ({onEmailSelect}) => {
                 });
                 const result = await res.json();
                 setMembers(result);
+
+                let initialList = {};
+
+                /* result is an array of objects */
+                result.forEach((obj) => {
+                    const [key, email] = Object.entries(obj);
+                    initialList = {...initialList, [key[1]]: {email:email[1],method: 'udw'}};
+                });
+
+                setSelectedValue(initialList);
             } catch (error) {
                 toast.error('Błąd ładowania książki adresów', {theme: 'colored'});
                 console.error(error);
@@ -24,6 +89,16 @@ export const EmailsList = ({onEmailSelect}) => {
         fetchMembers();
     }, []);
 
+    useEffect(() => {
+
+    }, [selectedValue]);
+
+    console.log(selectedValue);
+
+// Teraz możesz łatwo uzyskać dostęp do wartości za pomocą adresu e-mail
+//     const exampleEmail = 'pol9488@wp.pl';
+//     console.log(emailToValueMap.get(exampleEmail));
+
     return <>
         {members.map((member) => (
             <div key={member.id} className={styles.checkboxListRecord}>
@@ -32,10 +107,18 @@ export const EmailsList = ({onEmailSelect}) => {
                     type="checkbox"
                     value={member.email}
                     name={member.email}
-                    onChange={(event) => onEmailSelect(member.email, event.target.checked)}
+                    onChange={event => handleSelectChange(member.id, event.target.checked)}
                 />
                 <label htmlFor={member.id}>{member.email}</label>
                 <label htmlFor={member.id}>{member.description}</label>
+                <select
+                    value={selectedValue[member.id].method}
+                    onChange={event => handleSelectChange(member.id, event.target.value)}
+                >
+                    <option value="udw">UDW</option>
+                    <option value="1">Zwykły</option>
+                    <option value="dw">DW</option>
+                </select>
             </div>
         ))}
     </>
