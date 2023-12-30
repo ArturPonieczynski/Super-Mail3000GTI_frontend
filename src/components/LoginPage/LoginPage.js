74576ac51e6de5879278ff90ee962d68490734f7
Artur Ponieczynski <ponieczynski@gmail.com>
2023-12-30 23:36:27 +0100

    Refactor: Refactor LoginPage.js and styles sheet. Create handleInputChange function.

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -2,8 +2,9 @@ import React, {useState} from "react";
 import {apiUrl} from "../../config/api";
 import {toast} from "react-toastify";
 import {useNavigate} from "react-router-dom";
-import style from "./LoginPage.module.css";
+
 import logo from "../assets/images/main512.png";
+import style from "./LoginPage.module.css";
 
 export const LoginPage = () => {
 
@@ -14,6 +15,10 @@ export const LoginPage = () => {
 
     const navigate = useNavigate();
 
+    const handleInputChange = event => {
+        updateForm(event.target.name, event.target.value);
+    };
+
     const updateForm = (key, value) => {
         setForm(form => ({
             ...form,
@@ -21,56 +26,57 @@ export const LoginPage = () => {
         }));
     };
 
-    const sendForm = async (event) => {
+    const sendForm = async event => {
         event.preventDefault();
 
         try {
-            const res = await fetch(`${apiUrl}/api/login`, {
+            const loginResponse = await fetch(`${apiUrl}/api/login`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({...form}),
+                body: JSON.stringify(form),
             });
 
-            const result = await res.json();
+            const result = await loginResponse.json();
 
             if (result.error) {
-                toast.error(`${result.error}`);
-                return;
-            } if (result.response) {
+                return toast.error(`${result.error}`);
+            }
+            if (result.ok) {
                 navigate('/mail');
             }
 
         } catch (error) {
-            toast.error('Ups... Coś poszło nie tak ! Spróbuj ponownie za jakiś czas.');
-            navigate('/login');
+            return toast.error('Ups... Coś poszło nie tak ! Spróbuj ponownie za jakiś czas.');
         }
     };
 
-    return (<div className={style.divContainer}>
-        <img className={style.logo} src={logo} alt="Main logo"/>
-        <form className={style.loginForm} action="" onSubmit={sendForm}>
-            <input
-                className={style.input}
-                type="text"
-                name="name"
-                placeholder="Login"
-                maxLength={50}
-                value={form.name}
-                onChange={e => updateForm('name', e.target.value)}
-                required/>
-            <input
-                className={style.input}
-                type="password"
-                name="password"
-                placeholder="Password"
-                maxLength={45}
-                minLength={4}
-                value={form.password}
-                onChange={e => updateForm('password', e.target.value)}
-                required/>
-            <button className={style.formButton} type="submit">Login</button>
-        </form>
-    </div>)
+    return (
+        <div className={style.divContainer}>
+            <img className={style.logo} src={logo} alt="Main logo"/>
+            <form className={style.loginForm} onSubmit={sendForm}>
+                <input
+                    className={style.input}
+                    type="text"
+                    name="name"
+                    placeholder="Login"
+                    maxLength={50}
+                    value={form.name}
+                    onChange={handleInputChange}
+                    required/>
+                <input
+                    className={style.input}
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    maxLength={45}
+                    minLength={4}
+                    value={form.password}
+                    onChange={handleInputChange}
+                    required/>
+                <button className={style.formButton} type="submit">Login</button>
+            </form>
+        </div>
+    )
 };
