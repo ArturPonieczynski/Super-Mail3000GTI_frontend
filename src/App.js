d6110ec563426ab21613b3c5cdadfcff83cc1226
Artur Ponieczynski <ponieczynski@gmail.com>
2024-02-11 13:25:32 +0100

    Feature/refactor-routes: Created auto navigate from the main page to /login page.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import {LoginPage} from "./components/LoginPage/LoginPage";
 import {ToastContainer} from "react-toastify";
-import {Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import 'react-toastify/dist/ReactToastify.css';
 import {EmailFormPage} from "./components/EmailFormPage/EmailFormPage";
 import {Footer} from "./components/Footer/Footer";
 
 export function App() {
     return (<>
-        <ToastContainer theme="dark" position="top-center"/>
+        <ToastContainer theme='dark' position='top-center'/>
         <Routes>
-            <Route path='/' element={<LoginPage/>}/>
+            <Route path='/' element={<Navigate to='/login' replace/>}/>
             <Route path='/login' element={<LoginPage/>}/>
             <Route path='/email' element={<EmailFormPage/>}/>
         </Routes>
