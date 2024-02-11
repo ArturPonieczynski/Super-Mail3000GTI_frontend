4fb488aaf6e0a13acaa54a0efc911ead07a737be
Artur Ponieczynski <ponieczynski@gmail.com>
2024-02-11 18:59:05 +0100

    Feature/refactor-routes: Created LoginView.js and inserted it to App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import {LoginPage} from "./components/LoginPage/LoginPage";
-import {ToastContainer} from "react-toastify";
 import {Navigate, Route, Routes} from "react-router-dom";
-import 'react-toastify/dist/ReactToastify.css';
 import {EmailFormPage} from "./components/EmailFormPage/EmailFormPage";
-import {Footer} from "./components/Footer/Footer";
+import {LoginView} from "./views/LoginView";
+import {ToastContainer} from "react-toastify";
+
+import 'react-toastify/dist/ReactToastify.css';
 
 export function App() {
-    return (<>
+    return <>
         <ToastContainer theme='dark' position='top-center'/>
         <Routes>
             <Route path='/' element={<Navigate to='/login' replace/>}/>
-            <Route path='/login' element={<LoginPage/>}/>
+            <Route path='/login' element={<LoginView/>}/>
             <Route path='/email' element={<EmailFormPage/>}/>
         </Routes>
-        <Footer/>
-    </>);
+    </>;
 }
---
+++ src/views/LoginView.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {LoginPage} from "../components/LoginPage/LoginPage";
+import {Footer} from "../components/Footer/Footer";
+
+export const LoginView = () => (
+    <>
+        <LoginPage/>
+        <Footer/>
+    </>
+);
