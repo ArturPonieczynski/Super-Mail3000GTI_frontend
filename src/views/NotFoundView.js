3378145f9190b60465a83724d52c74f0868b92fe
Artur Ponieczynski <ponieczynski@gmail.com>
2024-02-12 16:31:58 +0100

    Feature/refactor-routes: Implement the anchor in the footer component of a NotFoundView.

--- src/views/NotFoundView.js
+++ src/views/NotFoundView.js
@@ -4,6 +4,8 @@ import {ErrorPage} from "../components/ErrorPage/ErrorPage";
 export const NotFoundView = () => (
     <>
         <ErrorPage/>
-        <Footer/>
+        <Footer>
+            <a href="https://www.freepik.com/free-vector/404-error-lost-space-concept-illustration_20602746.htm#query=lost&position=17&from_view=search&track=sph&uuid=f065a554-ab15-4acf-9a9b-8999b3f88614">Image by storyset</a> on Freepik
+        </Footer>
     </>
 );
