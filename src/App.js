f79dac0a0e861492ee3e59877dc715d78f005923
Artur Ponieczynski <ponieczynski@gmail.com>
2024-02-12 16:14:49 +0100

    Feature/refactor-routes: Created NotFoundView.js and implemented it in to App.js for all not existing paths.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import {Navigate, Route, Routes} from "react-router-dom";
 import {LoginView} from "./views/LoginView";
 import {EmailFormView} from "./views/EmailFormView";
-import {ToastContainer} from "react-toastify";
+import {NotFoundView} from "./views/NotFoundView";
 
+import {ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
 export function App() {
@@ -12,6 +13,7 @@ export function App() {
             <Route path='/' element={<Navigate to='/login' replace/>}/>
             <Route path='/login' element={<LoginView/>}/>
             <Route path='/email' element={<EmailFormView/>}/>
+            <Route path='/*' element={<NotFoundView/>}/>
         </Routes>
     </>
 }
---
+++ src/views/NotFoundView.js
@@ -0,0 +1,9 @@
+import {Footer} from "../components/Footer/Footer";
+import {ErrorPage} from "../components/ErrorPage/ErrorPage";
+
+export const NotFoundView = () => (
+    <>
+        <ErrorPage/>
+        <Footer/>
+    </>
+);
